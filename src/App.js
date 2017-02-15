1a8af5d84f13a5348a00c08e0c08e4a8a74790ce
KTseng <tgpsstar@gmail.com>
2017-02-15 17:20:21 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 // import { dev_FirebaseConfig } from './config'
 import RouterComponent from './Router';
-import Firestack from 'react-native-firestack';
+// import Firestack from 'react-native-firestack';
 
 class App extends Component {
   constructor() {
     super();
     // Initialize Firebase
     // const firebaseConfig = dev_FirebaseConfig;
-    const configurationOptions = {
-      debug: true,
-    };
-    const firestack = new Firestack(configurationOptions);
+    // const configurationOptions = {
+    //   debug: true,
+    // };
+    // const firestack = new Firestack(configurationOptions);
   }
 
   render() {
