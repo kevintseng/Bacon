fdba38bb8c34374ed474e8f63c96c181a9564c9b
KTseng <tgpsstar@gmail.com>
2017-04-14 14:37:40 +0800

    MeetCute view bug fixed

--- src/views/MeetCute.js
+++ src/views/MeetCute.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { View, Dimensions, ActivityIndicator } from 'react-native';
-import { observer } from 'mobx-react/native';
-import Moment from 'moment';
-import { Actions } from 'react-native-router-flux';// eslint-disable-line
-import Reactotron from 'reactotron-react-native'; // eslint-disable-line
-import DeviceInfo from 'react-native-device-info';
-import OthersProfile from './OthersProfile';
+import React, { Component } from "react";
+import { View, Dimensions, ActivityIndicator } from "react-native";
+import { observer } from "mobx-react/native";
+import Moment from "moment";
+import { Actions } from "react-native-router-flux"; // eslint-disable-line
+import Reactotron from "reactotron-react-native"; // eslint-disable-line
+import DeviceInfo from "react-native-device-info";
+import OthersProfile from "./OthersProfile";
 
-const width = Dimensions.get('window').with;
+const width = Dimensions.get("window").with;
 
 @observer
 export default class MeetCute extends Component {
@@ -17,106 +17,141 @@ export default class MeetCute extends Component {
     this.store = this.props.store;
     this.db = this.props.localdb;
     this.state = {
-      user: this.store.user,
       size: { width },
       list: null,
       next: null,
+      data: null,
       loading: true,
     };
     this.seekMeetQs(this.store.user.sexOrientation);
+    Reactotron.log(this.store.user.sexOrientation);
   }
 
   componentWillMount() {
-    Reactotron.debug('Rendering MeetCute');
-    Actions.refresh({ key: 'drawer', open: false });
+    Reactotron.debug("Rendering MeetCute");
+    Actions.refresh({ key: "drawer", open: false });
   }
 
   componentDidMount() {
-    Reactotron.debug('MeetCute rendered');
+    Reactotron.debug("MeetCute rendered");
     const deviceId = DeviceInfo.getUniqueID();
     const locale = DeviceInfo.getDeviceLocale();
     const country = DeviceInfo.getDeviceCountry();
-    Reactotron.log('Device ID: ' + deviceId + ', locale: ' + locale + ', country: ' + country);
+    Reactotron.log(
+      "Device ID: " + deviceId + ", locale: " + locale + ", country: " + country
+    );
   }
 
-  mq = (cond) => {
+  mq = cond => {
     const _list = [];
-    const query = this.firebase.database().ref(`seeking/${this.store.user.country}/${cond}`);
+    const query = this.firebase
+      .database()
+      .ref(`seeking/${this.store.user.country}/${cond}`);
 
-    query.once('value', snap => {
-      Reactotron.log('Executing mq cond:' + cond);
-      snap.forEach(childsnap => {
-        const _uid = childsnap.val().uid;
-        _list.push(_uid);
+    query
+      .once("value", snap => {
+        Reactotron.log("Executing mq cond:" + cond);
+        snap.forEach(childsnap => {
+          const _uid = childsnap.val().uid;
+          _list.push(_uid);
+        });
+        Reactotron.log("Print list");
+        Reactotron.log(_list);
+        this.setState({ list: _list });
+      })
+      .then(() => {
+        Reactotron.log(_list[0]);
+        this.getProfile(_list[0]);
       });
-      Reactotron.log('Print list');
-      Reactotron.log(_list);
-      this.setState({ list: _list });
-    }).then(() => {
-      Reactotron.log(_list[0]);
-      this.getProfile(_list[0]);
-    })
-  }
+  };
 
   getProfile = uid => {
-    const q = this.firebase.database().ref('users/' + uid);
-    q.once('value', snap => {
-      Reactotron.log('Profile data:' + snap.val());
+    const q = this.firebase.database().ref("users/" + uid);
+    q.once("value", snap => {
+      Reactotron.log("Profile data:");
+      Reactotron.log(snap.val());
       this.setState({
         data: snap.val(),
         loading: false,
       });
     });
-  }
+  };
 
-  seekMeetQs = (so) => {
-    switch(so) {
-      case 'msf':
-       this.mq('fsm');
-       break;
-      case 'msm':
-        this.mq('msm');
+  seekMeetQs = so => {
+    switch (so) {
+      case "msf":
+        this.mq("fsm");
         break;
-      case 'fsm':
-        this.mq('msf');
-       break;
-      case 'fsf':
-          this.mq('fsf');
+      case "msm":
+        this.mq("msm");
+        break;
+      case "fsm":
+        this.mq("msf");
+        break;
+      case "fsf":
+        this.mq("fsf");
         break;
     }
-  }
+  };
 
   getNext = () => {
     this.setState({
-      loading: true,
-    })
+      loading: true
+    });
     const uid = this.state.data.uid;
     const list = this.state.list;
-    Reactotron.log('MeetCute: getNext() pressed');
+    Reactotron.log("MeetCute: getNext() pressed");
     const _index = list.indexOf(uid) + 1;
     Reactotron.log(_index);
-    if(list.length > _index) {
+    if (list.length > _index) {
       this.getProfile(list[_index]);
     } else {
-      Reactotron.log('This is the last user');
+      this.setState({
+        loading: false,
+      });
+      Reactotron.log("This is the last user");
     }
-  }
+  };
 
   handleLike = uid => {
-    const r = this.firebase.database().ref('users/' + uid + '/likes');
+    const r = this.firebase.database().ref("users/" + uid + "/likes");
     const d = {
       uid,
-      timestamp: Moment().unix(),
+      timestamp: Moment().unix()
     };
     r.set(d);
-  }
+  };
 
   render() {
-    const indicator = (<ActivityIndicator style={{ alignItems: 'center', justifyContent: 'center', padding: 8, marginTop: 150 }} size='large' />);
-    
+    const indicator = (
+      <ActivityIndicator
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 8,
+          marginTop: 150
+        }}
+        size="large"
+      />
+    );
+
+    // <OthersProfile
+    //   data={this.state.data}
+    //   getNext={this.getNext}
+    //   handleLike={this.handleLike}
+    // />
+    Reactotron.log('this.state.data');
+    Reactotron.log(this.state.data);
     return (
       <View>
-        <OthersProfile data={this.state.data} getNext={this.getNext} handleLike={this.handleLike}/>
+        {this.state.loading && indicator}
+        {
+          this.state.data && !this.state.loading && <OthersProfile
+            data={this.state.data}
+            getNext={this.getNext}
+            handleLike={this.handleLike}
+          />
+        }
       </View>
     );
   }
--- src/views/Signup/Signup4.js
+++ src/views/Signup/Signup4.js
@@ -1,48 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
-    View,
-    Dimensions,
-    Text,
-    ActivityIndicator,
-    Platform,
-  } from 'react-native';
-import Moment from 'moment';
-import DeviceInfo from 'react-native-device-info';
-import ImageResizer from 'react-native-image-resizer'; //eslint-disable-line
-import { Card, Button, Avatar } from 'react-native-elements';
-import { Actions } from 'react-native-router-flux';
-import RNFetchBlob from 'react-native-fetch-blob';
-import { observer } from 'mobx-react/native';
-import ImagePicker from 'react-native-image-picker';
-import Reactotron from 'reactotron-react-native';
-import { Header } from '../../components/Header';
+  View,
+  Dimensions,
+  Text,
+  ActivityIndicator,
+  Platform
+} from "react-native";
+import Moment from "moment";
+import DeviceInfo from "react-native-device-info";
+import ImageResizer from "react-native-image-resizer"; //eslint-disable-line
+import { Card, Button, Avatar } from "react-native-elements";
+import { Actions } from "react-native-router-flux";
+import RNFetchBlob from "react-native-fetch-blob";
+import { observer } from "mobx-react/native";
+import ImagePicker from "react-native-image-picker";
+import Reactotron from "reactotron-react-native";
+import { Header } from "../../components/Header";
 
-
-const {width, height} = Dimensions.get('window'); //eslint-disable-line
+const { width, height } = Dimensions.get("window"); //eslint-disable-line
 
 const styles = {
-  wrapper: { alignSelf: 'center', height: 150, width: 150, marginBottom: 10 },
+  wrapper: { alignSelf: "center", height: 150, width: 150, marginBottom: 10 }
 };
 
 const ipOptions = {
-  mediaType: 'photo',
+  mediaType: "photo",
   maxWidth: 1000,
   maxHeight: 1000,
   quality: 0.8,
   noData: false,
   storageOptions: {
     skipBackup: true,
-    path: 'hookup'
-  },
+    path: "hookup"
+  }
 };
 
-const Blob = RNFetchBlob.polyfill.Blob
-const fs = RNFetchBlob.fs
-window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
-window.Blob = Blob
+const Blob = RNFetchBlob.polyfill.Blob;
+const fs = RNFetchBlob.fs;
+window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
+window.Blob = Blob;
 
-@observer
-class Signup4 extends Component {
+@observer class Signup4 extends Component {
   constructor(props) {
     super(props);
     this.sustore = this.props.sustore;
@@ -50,113 +48,120 @@ class Signup4 extends Component {
     this.firebase = this.props.fire;
     this.state = {
       size: {
-          width,
-          height: 600
+        width,
+        height: 600
       },
-      country: DeviceInfo.getDeviceCountry(),
+      // country: DeviceInfo.getDeviceCountry(),
+      country: 'TW', //初期暫時都預設為台灣
       locale: DeviceInfo.getDeviceLocale(),
       imageHeight: null,
       imageWidth: null,
       imageTimestamp: null,
       imageGeo: null,
       photoUrl: null,
-      loading: false,
+      loading: false
     };
   }
 
   componentWillMount() {
-    Reactotron.log('Will mount Signup4');
+    Reactotron.log("Will mount Signup4");
   }
 
   componentDidMount() {
     this.store.setInSignupProcess(true);
-    Reactotron.log('Signup 4 mounted');
+    Reactotron.log("Signup 4 mounted");
   }
 
   addImage = () => {
-    ImagePicker.showImagePicker(ipOptions, async (res) => {
+    ImagePicker.showImagePicker(ipOptions, async res => {
       this.setState({
         loading: true
       });
-      if(res.didCancel) {
-        Reactotron.log('ImagePicker: User cancelled image picker');
+      if (res.didCancel) {
+        Reactotron.log("ImagePicker: User cancelled image picker");
         return;
-      } else if(res.error) {
-        Reactotron.error('ImagePicker Error: ' + res.error);
+      } else if (res.error) {
+        Reactotron.error("ImagePicker Error: " + res.error);
         this.setState({
-          loading: false,
+          loading: false
         });
       } else {
-        ImageResizer.createResizedImage(res.uri, 200, 200, 'JPEG', 80)
-        .then( async (resizedImageUri) => {
-          Reactotron.log('resizedImageUri: ' + resizedImageUri);
-          this.uploadImage(resizedImageUri);
-          this.setState({
-            image: 'file://' + resizedImageUri,
-            imageGeo: {
-              lon: res.longitude,
-              lat: res.latitude
-            },
-            imageTimestamp: res.timestamp,
+        ImageResizer.createResizedImage(res.uri, 200, 200, "JPEG", 80)
+          .then(async resizedImageUri => {
+            Reactotron.log("resizedImageUri: " + resizedImageUri);
+            this.uploadImage(resizedImageUri);
+            this.setState({
+              image: "file://" + resizedImageUri,
+              imageGeo: {
+                lon: res.longitude,
+                lat: res.latitude
+              },
+              imageTimestamp: res.timestamp
+            });
+          })
+          .catch(err => {
+            Reactotron.error(err);
           });
-        }).catch((err) => {
-          Reactotron.error(err);
-        });
       }
     });
-  }
+  };
 
   generateFilename = () => {
-    return this.firebase.database().ref('users/' + this.store.user.uid + '/photos').push().key;
-  }
+    return this.firebase
+      .database()
+      .ref("users/" + this.store.user.uid + "/photos")
+      .push().key;
+  };
 
-  uploadImage = (uri, ref, mime = 'image/jpeg') => {
-    Reactotron.debug('Uploading image');
-    const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
+  uploadImage = (uri, ref, mime = "image/jpeg") => {
+    Reactotron.debug("Uploading image");
+    const uploadUri = Platform.OS === "ios" ? uri.replace("file://", "") : uri;
     let uploadBlob = null;
     // const filename = this.generateFilename();
-    const imageRef = this.firebase.storage().ref('avatars/' + this.sustore.uid + '.jpg');
-    fs.readFile(uploadUri, 'base64')
-      .then((data) => {
+    const imageRef = this.firebase
+      .storage()
+      .ref("avatars/" + this.sustore.uid + ".jpg");
+    fs
+      .readFile(uploadUri, "base64")
+      .then(data => {
         return Blob.build(data, { type: `${mime};BASE64` });
       })
-      .then((blob) => {
-        uploadBlob = blob
-        return imageRef.put(blob, { contentType: mime })
+      .then(blob => {
+        uploadBlob = blob;
+        return imageRef.put(blob, { contentType: mime });
       })
       .then(() => {
-        uploadBlob.close()
+        uploadBlob.close();
         return imageRef.getDownloadURL();
       })
-      .then((url) => {
-        Reactotron.debug('Url');
+      .then(url => {
+        Reactotron.debug("Url");
         Reactotron.debug(url);
         this.setState({
           photoUrl: url,
-          loading: false,
+          loading: false
         });
         this.sustore.setAvatar(url);
       })
       .catch(err => {
-        Reactotron.error('ReadFile error: ');
+        Reactotron.error("ReadFile error: ");
         Reactotron.error(err);
-      })
-  }
+      });
+  };
 
   handleSubmit = async () => {
-    if(!this.state.photoUrl) {
-      alert('請給偶一張您的美/帥照, 拜偷拜偷');
+    if (!this.state.photoUrl) {
+      alert("請給偶一張您的美/帥照, 拜偷拜偷");
       return;
     }
     try {
-
       const user = await this.firebase.auth().currentUser;
       const analysis = {
         charm: 50,
         friendliness: 50,
         likeness: 50,
         popularity: 50,
-        activity: 50,
+        activity: 50
       };
 
       const postData = {
@@ -175,55 +180,73 @@ class Signup4 extends Component {
         country: this.state.country,
         photoVerified: false,
         vip: false,
-        bio: '',
-        hobby: '',
-        lang: '',
+        bio: "",
+        hobby: "",
+        lang: "",
         gallery: [],
         analysis,
         signupCompleted: true,
         created: this.sustore.created,
-        lastUpdated: Moment().unix(),
+        lastUpdated: this.sustore.lastUpdated
       };
 
-      await user.updateProfile({
-        photoURL: this.state.photoUrl,
-        displayName: this.sustore.nickname,
-      }).then(() => {
-        Reactotron.debug('User profile updated');
-      }).catch(err => {
-        Reactotron.error('User profile update error');
-        Reactotron.error(err);
-      });
+      await user
+        .updateProfile({
+          photoURL: this.state.photoUrl,
+          displayName: this.sustore.nickname
+        })
+        .then(() => {
+          Reactotron.debug("User profile updated");
+        })
+        .catch(err => {
+          Reactotron.error("User profile update error");
+          Reactotron.error(err);
+        });
 
-      await this.firebase.database().ref(`users/${this.sustore.uid}`).set(postData).then((res) => {
-        Reactotron.debug('Set user data to firebase');
-        Reactotron.debug(res);
-      }).catch(err => {
-        Reactotron.error('Set user data failed');
-        Reactotron.error(err);
-      });
+      await this.firebase
+        .database()
+        .ref(`users/${this.sustore.uid}`)
+        .set(postData)
+        .then(res => {
+          Reactotron.debug("Set user data to firebase");
+          Reactotron.debug(res);
+        })
+        .catch(err => {
+          Reactotron.error("Set user data failed");
+          Reactotron.error(err);
+        });
 
       //Add user to seeking table on firebase
-      const soRef = 'seeking/' + this.state.country + '/' + this.sustore.sexOrientation + '/' + this.sustore.uid;
+      const soRef = "seeking/" +
+        this.state.country +
+        "/" +
+        this.sustore.sexOrientation +
+        "/" +
+        this.sustore.uid;
       const soData = {
         uid: this.sustore.uid
       };
 
       Reactotron.log(soRef);
       Reactotron.log(soData);
-      await this.firebase.database().ref(soRef).set(soData).then((res) => {
-        Reactotron.debug('Set sexOrientation to firebase');
-        Reactotron.debug(res);
-      }).catch(err => {
-        Reactotron.error('Set sexOrientation failed');
-        Reactotron.error(err);
-      });
+      await this.firebase
+        .database()
+        .ref(soRef)
+        .set(soData)
+        .then(res => {
+          Reactotron.debug("Set sexOrientation to firebase");
+          Reactotron.debug(res);
+        })
+        .catch(err => {
+          Reactotron.error("Set sexOrientation failed");
+          Reactotron.error(err);
+        });
       this.store.setInSignupProcess(false);
-      return Actions.sessioncheck();
+      Actions.sessioncheck();
     } catch (err) {
       Reactotron.error(err);
     }
-  }
+  };
 
   render() {
     const { nickname } = this.sustore;
@@ -231,43 +254,52 @@ class Signup4 extends Component {
     return (
       <View style={this.state.size}>
         <Header
-          headerText='上傳個人照'
-          rightButtonText='完成'
+          headerText="上傳個人照"
+          rightButtonText="完成"
           onRight={this.handleSubmit}
-          rightColor='#007AFF'
+          rightColor="#007AFF"
           onLeft={() => Actions.pop()}
-          leftColor='#007AFF'
+          leftColor="#007AFF"
         />
         <View style={{ marginBottom: 20 }}>
           <Card>
             <View style={styles.wrapper}>
-            {
-              this.state.loading ? <ActivityIndicator size='large' style={{
-                marginTop: 60
-              }}/> : <Avatar
-              xlarge
-              rounded
-              title={nickname}
-              source={{uri: this.state.image}}
-              />
-            }
+              {this.state.loading
+                ? <ActivityIndicator
+                    size="large"
+                    style={{
+                      marginTop: 60
+                    }}
+                  />
+                : <Avatar
+                    xlarge
+                    rounded
+                    title={nickname}
+                    source={{ uri: this.state.image }}
+                  />}
             </View>
-            <Text style={{marginBottom: 10}}>
+            <Text style={{ marginBottom: 10 }}>
               根據最新英國研究報告, 在交友app上顯示您的個人照片會提高配對成功機率喔！
-              無論你信不信, 反正我信了。</Text>
+              無論你信不信, 反正我信了。
+            </Text>
             <Button
-              icon={{name: 'add'}}
-              backgroundColor='#03A9F4'
-              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-              title='新增個人照片'
+              icon={{ name: "add" }}
+              backgroundColor="#03A9F4"
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0
+              }}
+              title="新增個人照片"
               onPress={this.addImage}
             />
           </Card>
           <Button
             style={{ marginTop: 10 }}
-            backgroundColor='transparent'
-            color='#007AFF'
-            title={'完成'}
+            backgroundColor="transparent"
+            color="#007AFF"
+            title={"完成"}
             onPress={this.handleSubmit}
           />
         </View>
