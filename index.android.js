5891b251126c68d8bf6ebfd950d4d27cd6591db8
frank <frank@kayming.com>
2017-04-17 15:44:00 +0800

    add apk

--- index.android.js
+++ index.android.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
-import './ReactotronConfig';
+//import './ReactotronConfig';
 
 AppRegistry.registerComponent('hookup', () => App);
