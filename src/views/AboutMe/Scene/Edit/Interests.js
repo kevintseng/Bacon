9720a846e6e9ff8a236470663532b3eab44a2c34
Tsao <tsaohucn@gmail.com>
2017-05-09 02:35:33 +0800

    modify interests components

--- src/views/AboutMe/Scene/Edit/Interests.js
+++ src/views/AboutMe/Scene/Edit/Interests.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import { Alert, Button } from 'react-native';
-
+import { TextInput, View } from 'react-native';
+import ChangeColorButton from './ChangeColorButton'
+import { Icon } from 'react-native-elements'
 
 class Interests extends Component {
+
   constructor(props) {
-    super(props);
-  
-    this.state = {color: "#b0c4de"};
+    super(props)
+    this.state = { showLists: [], defaultLists: ["旅遊","健身","逛街","美食","網購","遊戲","路跑","電影"] }
   }
 
-  onPressLearnMore = () => {
-    //Alert.alert('Button has been pressed!');
-    this.setState = {color: "#008000"};
+  onKeyPress = () => {
+    this.setState({ showLists: this.state.showLists.concat(this.state.text) })
   }
+
+  renderButton = (list) => { return <ChangeColorButton key = { list } initButtonColor = "#ffffff" changedButtonColor = "#000000" initTextColor = "#696969" changedTextColor = "#ffffff" title = { list } /> }
+
+  renderShowLists = () => {
+    return this.state.showLists.map(this.renderButton)
+  }
+
+  renderdefaultLists = () => {
+    return this.state.defaultLists.map(this.renderButton)
+  }  
 
   render(){
     return(
-      <Button color={this.state.color} onPress={this.onPressLearnMore} title="Learn More" accessibilityLabel="Learn more about this purple button" />
+      <View>
+        <View>
+          { this.renderShowLists() }
+        </View>
+        <View>
+          <Icon name = "border-color" color = "#000000" size = { 20 } /> 
+          <TextInput
+            onSubmitEditing = { this.onKeyPress }     
+            onChangeText = { (text) => this.setState({ text }) }
+            value = { this.state.text }
+          />      
+        </View>
+        <View>
+          { this.renderdefaultLists() }
+        </View>         
+      </View>  
     )
   }
 }
-
 
 export default Interests;
