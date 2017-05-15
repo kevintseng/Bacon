0a3f113306973f17dcb973cea21fea2151ff6049
Tsao <tsaohucn@gmail.com>
2017-05-15 18:09:44 +0800

    change SideBar to stateless function

--- src/views/SideBar.js
+++ src/views/SideBar.js
@@ -1,159 +1,71 @@
-import React, { Component } from 'react';
-import { ScrollView, Dimensions, Alert } from 'react-native';
+import React from 'react'
+import { ScrollView, Dimensions } from 'react-native'
 
-import { List, ListItem, Button } from 'react-native-elements';
-import { Actions } from 'react-native-router-flux';
-import { observer } from 'mobx-react/native';
-import { SIDEBAR_LINKS } from '../Configs';
+import { List, ListItem } from 'react-native-elements'
+//import { Actions } from 'react-native-router-flux'
+import { observer } from 'mobx-react/native'
+import { SIDEBAR_LINKS } from '../Configs'
+//const loading = require('../images/loading.gif')
 
-const { height } = Dimensions.get('window'); //eslint-disable-line
+const { height } = Dimensions.get('window') //eslint-disable-line
 
-const list = SIDEBAR_LINKS;
-const loading = require('../images/loading.gif');
+const list = SIDEBAR_LINKS
 
 const styles = {
-  containerStyle: {
+  scrollView: {
+    height,
+    backgroundColor: '#ffffff'
+  },  
+  listContainerStyle:{
+    marginTop: 0, 
     borderBottomWidth: 0,
+    backgroundColor: '#ffffff'
+  },
+  listItemContainerStyle: {
+    borderBottomWidth: 0
   },
   wrapperStyle: {
-    paddingLeft: 43,
-  }
-};
-
-@observer
-export default class SideBar extends Component {
-  constructor(props) {
-    super(props);
-    this.store = this.props.store;
-    this.firebase = this.props.fire;
-    this.db = this.props.localdb;
-  }
-
-  componentWillMount() {
-    console.log('Rendering SideBar.');
-  }
-
-  componentDidMount() {
-    console.log('SideBar rendered.');
-  }
-
-  handleImageChange = () => {
-    // Do something
-    console.log('handleImageChange pressed.');
-  };
-
-  badgeShow = (newCount) => {
-    if (newCount > 0) {
-      return { value: newCount, badgeContainerStyle: { backgroundColor: 'red' } };
-    }
-    return false;
-  };
-
-  signOut = () => {
-    // Clear out appstore's user data
-    if(this.store.user) {
-      this.setOffline(this.store.user.uid);
-      this.store.signOut();
-    }
-
-    // Sign out from firebase
-    this.firebase.auth().signOut();
-
-    // Clear out local database's user data
-    this.db.remove({
-      key: 'user',
-    });
-
-    // Render SessionCheck and redirect to signin view
-    Actions.sessioncheck({type: 'reset'});
-  }
-
-  setOffline(uid) {
-    // const timestamp = Math.floor(Date.now() / 1000);
-    this.firebase.database().ref('/online/' + uid).remove();
-  }
-
-  handleOnPress(key) {
-    switch (key) {
-      case 'meetcute':
-        return () => Actions.meetcute({type: 'reset'});
-      case 'nearby':
-        return () => Actions.nearby({type: 'reset'});
-      case 'favorites':
-        return () => Actions.favorites({type: 'reset'});
-      case 'visitors':
-        return () => Actions.visitors({type: 'reset'});
-      case 'likesyou':
-        return () => Actions.likesyou({type: 'reset'});
-      case 'messages':
-        return () => Actions.messages({type: 'reset'});
-      case 'settings':
-        return () => Actions.settings_wrapper({type: 'reset'});
-      case 'aboutMeRoutes':
-      // Go to profile view only when user data is loaded.
-        if(this.store.user != null && this.store.user != '') {
-          return () => Actions.aboutMeRoutes({type: 'reset'});
-        }
-        return () => {};
-    }
-  }
-
-  render() {
-
-    let displayName, photoURL;
-    if(this.store.user != null && this.store.user != '') {
-      displayName = this.store.user.displayName;
-      photoURL = { uri: this.store.user.photoURL };
-    } else {
-      displayName = '載入中...';
-      photoURL = loading;
-    }
-
-    console.log('Height: ' + height);
-
-    return (
-      <ScrollView
-        style={{
-          height,
-          backgroundColor: '#F5F5F5'
-        }}
-      >
-        <List>
-          <ListItem
-            containerStyle={{ height: 43, backgroundColor: '#F5F5F5', borderBottomWidth: 0.5, borderBottomColor: '#BDBDBD'}}
-            rightIcon={{ name: 'menu', color: 'black' }}
-            onPress={() => Actions.refresh({ key: 'drawer', open: false })}
-          />
-          <ListItem
-            roundAvatar
-            containerStyle={styles.containerStyle}
-            avatar={ photoURL }
-            title={ displayName }
-            onPress={this.handleOnPress('aboutMeRoutes')}
-          />
-          {
-            list.map((item, i) => (
-              <ListItem
-                key={i}
-                containerStyle={styles.containerStyle}
-                wrapperStyle={styles.wrapperStyle}
-                underlayColor={'#f8f8f8'}
-                title={item.title}
-                leftIcon={{ name: item.icon }}
-                badge={this.badgeShow(item.new)}
-                onPress={this.handleOnPress(item.key)}
-              />
-            ))
-          }
-        </List>
-        <Button
-          style={{ marginTop: 10 }}
-          color={'black'}
-          backgroundColor={'#F5F5F5'}
-          title={'登出'}
-          onPress={this.signOut}
-        />
-      </ScrollView>
-    );
+    paddingLeft: 43
   }
 }
+
+const SideBar = observer(({ store }) => {
+
+  const { scrollView, listContainerStyle, listItemContainerStyle, wrapperStyle } = styles
+
+  return(
+    <ScrollView style = { scrollView } >
+      <List containerStyle = { listContainerStyle } >
+        <ListItem
+            containerStyle = { [listItemContainerStyle,{height: 53, borderBottomWidth: 0.5, borderBottomColor: '#808080'}] }
+            rightIcon = {{name: 'menu', color: 'black'}}  
+            //onPress         
+        />
+        <ListItem
+            roundAvatar
+            containerStyle = { listItemContainerStyle }
+            avatar = { store.user.photoURL }
+            title = { store.user.displayName }  
+            //onPress 
+        />
+        {
+          list.map((item, i) => (
+              <ListItem
+                key = { i }
+                containerStyle = { listItemContainerStyle }
+                wrapperStyle = { wrapperStyle }
+                underlayColor = { '#f8f8f8' }
+                title = { item.title }
+                leftIcon = {{name: item.icon}}
+                badge = { false }
+                //onPress
+              />
+          ))
+        }
+      </List>
+    </ScrollView>
+  )
+ }
+)
+
+export { SideBar }
