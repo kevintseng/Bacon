31295d907bf25b2b8d8cc17fbf1cefdf517e1303
Tsao <tsaohucn@gmail.com>
2017-05-18 14:57:18 +0800

    migrate login to prey stor sucuessfully

--- src/store/Prey.js
+++ src/store/Prey.js
@@ -1,27 +1,58 @@
-import { observable, action, useStrict, autorun } from 'mobx'
-import { Actions } from 'react-native-router-flux'
+import { observable, action, useStrict } from 'mobx'
+//import { Actions } from 'react-native-router-flux'
+//import { Alert } from "react-native";
 //import DeviceInfo from "react-native-device-info"
 // import autobind from 'autobind-decorator';
-useStrict(true)
+useStrict(false)
 // @autobind
 
 class Prey {
-  @observable users;
+  @observable user;
+  @observable list;
+  @observable loading;
 
   constructor(firebase) {
-    this.prey = null;
+    this.list = []
+    this.user = {}
+    this.loading = true
     this.firebase = firebase
   }
 
-  @action grepUsers(sexOrientation){
+  @action grepLists(sexOrientation){
     //const deviceId = DeviceInfo.getUniqueID();
     //const locale = DeviceInfo.getDeviceLocale();
     //const country = DeviceInfo.getDeviceCountry();
     this.seekMeetQs(sexOrientation)
+    //console.warn(this.list)
   }
 
+  @action getNext = () => {
+    //alert("Next")
+    //console.warn(this.loading)
+    this.loading = true
+    //this.setState({
+    //  loading: true
+    //});
+    //console.warn(this.user)
+    const uid = this.user.uid;
+    const list = this.list;
+    //console.log("MeetCute: getNext() pressed");
+    const _index = list.indexOf(uid) + 1;
+    //console.log(_index);
+    if (list.length > _index) {
+      this.getProfile(list[_index]);
+    } else {
+      //this.setState({
+      //  loading: false,
+      //});
+      this.loading = false
+      alert('這是最後一位了, 在沒有有fu的對象我也沒辦法惹...GG');
+      //console.log("This is the last user");
+    }
+  }  
 
-  seekMeetQs(sexOrientation) {
+
+  @action seekMeetQs(sexOrientation) {
     switch (sexOrientation) {
       case "msf":
         this.mq("fsm");
@@ -38,44 +69,49 @@ class Prey {
     }
   }
 
-  mq(cond) {
+  @action mq(cond) {
     cond = 'test'
-    const _list = [];
+    //console.warn(cond)
+    //const _list = [];
     const query = this.firebase.database().ref("users")
     //console.warn(query)
       //.database()
       //.ref(`seeking/${this.store.user.country}/${cond}`);
     //ref.orderByKey().equalTo(cond,"sexOrientation")
-    query.orderByChild("sexOrientation").equalTo(cond).once("value", snap => {
+    //console.warn(query)
+    query.orderByChild("sexOrientation").equalTo('test').once("value", snap => {
         //console.log("Executing mq cond:" + cond);
         snap.forEach(childsnap => {
-          if (childsnap.val().country === 'Taiwan')
-          {
+          //if (childsnap.val().country === 'Taiwan')
+          //{
             const _uid = childsnap.val().uid;
-            _list.push(_uid);
-          }
+            //console.warn(_uid)
+            this.list.push(_uid);
+          //}
         });
-        console.log("Print list");
-        console.log(_list);
-        this.setState({ list: _list });
+        //console.log("Print list");
+        //console.log(_list);
+        //this.setState({ list: this.list });
       })
       .then(() => {
         //console.warn(_list[0]);
-        this.getProfile(_list[0]);
+        this.getProfile(this.list[0]);
       })
   }
 
-  getProfile(uid) {
+  @action getProfile(uid) {
     const q = this.firebase.database().ref("users/" + uid);
     q.once("value", snap => {
-      this.prey = snap.val()
+      this.user = snap.val()
+      //console.warn(this.user)
+      this.loading = false
+      //console.warn(this.user)
       //this.setState({
       //  data: snap.val(),
       //  loading: false,
       //})
     })
-  }
-   
+  }   
 }
 
 export default Prey;
