362d39300364add0a6112c5ebc286f4e30a451f2
Tsao <tsaohucn@gmail.com>
2017-05-18 17:54:29 +0800

    complete distance function

--- src/views/MeetCute/MeetCute/Index/Distance.js
+++ src/views/MeetCute/MeetCute/Index/Distance.js
@@ -2,14 +2,20 @@ import React from "react"
 import { View, Text } from "react-native"
 import { observer, inject } from "mobx-react/native"
 import { Icon } from 'react-native-elements'
+import geolib from 'geolib'
 
+const Distance = inject("prey","store")(observer(({ prey, store }) => {
 
-const Distance = inject("store")(observer(({ store }) => {
+  const distance = geolib.getDistance(
+      {latitude: store.user.geocode.lat, longitude: store.user.geocode.lng},
+      {latitude: prey.user.geocode.lat, longitude: prey.user.geocode.lng}
+  )
+
   return(
     <View style={{flexDirection: 'row', margin: 10}}>
      <Icon name='person-pin-circle'></Icon>
       <Text>
-        你們距離大約 7.9 公里
+        你們距離大約 {distance/1000} 公里
       </Text>
     </View>
   )
