f0ddce80ad71de059e3e1c13e32d7911605f4d99
Tsao <tsaohucn@gmail.com>
2017-05-19 18:23:05 +0800

    add fate page

--- src/Router.js
+++ src/Router.js
@@ -7,14 +7,12 @@ import { Icon } from "react-native-elements";
 import * as Firebase from "firebase"; // eslint-disable-line
 import MeetCute from "./views/MeetCute/MeetCute";
 import MeetChanceContainer from "./views/MeetChanceContainer";
+import FateContainer from "./views/FateContainer";
 import Messages from "./views/Messages";
-import LikesYou from "./views/LikesYou";
-import Visitors from "./views/Visitors";
 import Settings from "./views/Settings";
 import Signin from "./views/Signin";
 import SessionCheck from "./views/SessionCheck";
 import Chat from "./views/Chat";
-import Favorites from "./views/Favorites";
 import { Signup1, Signup2, Signup3, Signup4 } from "./views/Signup";
 import DrawerPanel from "./views/DrawerPanel";
 import ErrorView from "./views/ErrorView";
@@ -266,12 +264,6 @@ export default class RouterComponent extends Component {
                 title="巧遇"
                 renderLeftButton={menuButton}
               />
-              <Scene //喜歡您
-                key="favorites"
-                component={Favorites}
-                title="Favorites"
-                renderLeftButton={menuButton}
-              />
               <Scene //訊息
                 key="messages"
                 component={Messages}
@@ -279,17 +271,12 @@ export default class RouterComponent extends Component {
                 renderLeftButton={menuButton}
                 hideTabBar
               />
-              <Scene
-                key="likesyou"
-                component={LikesYou}
-                title="LikesYou"
+              <Scene //緣分
+                key="fate"
+                component={FateContainer}
+                title='緣分'
                 renderLeftButton={menuButton}
-              />
-              <Scene
-                key="visitors"
-                component={Visitors}
-                title="Visitors"
-                renderLeftButton={menuButton}
+                hideTabBar
               />
               <Scene
                 key="settings"
