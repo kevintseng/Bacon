81fbd5df87cf99a4129b6848311f99b8a10402b0
Tsao <tsaohucn@gmail.com>
2017-05-22 19:58:31 +0800

    complete meet_chance fetch data function

--- src/views/MeetChanceContainer/MeetChance.js
+++ src/views/MeetChanceContainer/MeetChance.js
@@ -1,41 +1,27 @@
 import React from 'react';
 import { View, ScrollView, Dimensions } from 'react-native'
 import { Cookie } from './MeetChance/Cookie'
+import { observer, inject } from "mobx-react/native"
+
 
 const { width, height } = Dimensions.get('window')
 
-const MeetChance = () => {
+const MeetChance = inject("prey")(observer(({ prey }) => {
+
+  const renderCookie = prey.preyList.map((prey)=>( <Cookie name={prey.displayName} photoURL={prey.photoURL} key={prey.uid}/> ))
+
   return(
     <View>
       <ScrollView style={{width, height}}>
-          <View style = {{backgroundColor: "#e6e6fa"}}>
+          <View style = {{backgroundColor: "#e6e6fa", justifyContent: 'center', alignItems: 'center'}}>
             <Cookie/>
           </View>
           <View style = {{flexDirection: 'row', flexWrap: 'wrap', alignItems:'flex-start', backgroundColor: "#ff0000"}}> 
-            <Cookie/>
-            <Cookie/>  
-            <Cookie/>  
-            <Cookie/>
-            <Cookie/>  
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
-            <Cookie/>
+            { renderCookie }
           </View>        
       </ScrollView>
     </View>
   )
-}
+}))
 
 export { MeetChance }
