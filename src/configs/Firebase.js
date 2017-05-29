bbb49043368b6344513bc9c49aca4a906e7ca32a
Tsao <tsaohucn@gmail.com>
2017-05-30 06:53:34 +0800

    modify architecture

--- src/configs/Firebase.js
+++ src/configs/Firebase.js
@@ -9,7 +9,7 @@ const FirebaseConfig = { //eslint-disable-line
   googleAppId: '1:725652377518:ios:bbdec425ca56934d',
   APIKey: 'AIzaSyBm9fcnGrmdAZo26_PJH5ieA0paXpLpYHY',
   GCMSenderID: '725652377518',
-};
+}
 
   // Firebase development configs
 const dev_FirebaseConfig = { //eslint-disable-line
@@ -19,37 +19,6 @@ const dev_FirebaseConfig = { //eslint-disable-line
   databaseURL: "https://rainyday-7fa26.firebaseio.com",
   storageBucket: "rainyday-7fa26.appspot.com",
   messagingSenderId: "309937705062"
-};
+}
 
-const SIDEBAR_LINKS = [
-  {
-    title: '邂逅',
-    icon: 'favorite',
-    key: 'meetcute',
-  },
-  {
-    title: '巧遇',
-    icon: 'wifi-tethering',
-    key: 'nearby',
-  },
-  {
-    title: '訊息',
-    icon: 'chat',
-    key: 'messages',
-    new: 1,
-  },
-  {
-    title: '緣分',
-    icon: 'explore',
-    key: 'fate',
-    new: 2,
-  },
-  {
-    title: '設定',
-    icon: 'settings',
-    key: 'settings',
-    new: 5,
-  }
-];
-
-export { FirebaseConfig, dev_FirebaseConfig, SIDEBAR_LINKS };
+export { FirebaseConfig, dev_FirebaseConfig }
