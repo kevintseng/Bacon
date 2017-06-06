34a528a0788efe823cac05b624728175ae965354
Tsao <tsaohucn@gmail.com>
2017-06-06 18:51:43 +0800

    complete setting button

--- src/app/views/MeetCute/Interests.js
+++ src/app/views/MeetCute/Interests.js
@@ -5,7 +5,7 @@ import { Badge } from 'react-native-elements'
 
 const Interests = inject("ObjectStore")(observer(({ ObjectStore }) => {
 
-  const renderInterests = ObjectStore.interests.map((interest) => (<Badge key={interest} value={interest} textStyle={{fontSize: 10, color: "#000000"}} containerStyle={{ borderWidth: 0.5, borderColor: '#000000', backgroundColor: '#ffffff', width: 50, marginRight: 10}}/>))
+  const renderInterests = ObjectStore.interests.map((interest) => (<Badge key={interest} value={interest} textStyle={{fontSize: 10, color: "#000000"}} containerStyle={{ borderWidth: 0.5, borderColor: '#000000', backgroundColor: '#ffffff', marginRight: 10}}/>))
 
   return(
     <View style={{flexDirection: 'row', margin: 10}}>
