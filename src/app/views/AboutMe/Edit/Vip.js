1b886f08ea6d58c9576452befeafea6fd3a10aa4
Tsao <tsaohucn@gmail.com>
2017-06-16 01:37:38 +0800

    sucuss conect android app store

--- src/app/views/AboutMe/Edit/Vip.js
+++ src/app/views/AboutMe/Edit/Vip.js
@@ -1,7 +1,30 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Button } from 'react-native'
 import { observer, inject } from 'mobx-react/native'
 import { CheckBox } from 'react-native-elements'
+import InAppBilling from "react-native-billing"
+
+
+const pay = async() => {
+  const productId = 'android.test.purchased'
+  await InAppBilling.close();
+  try {
+    await InAppBilling.open();
+    if (!await InAppBilling.isPurchased(productId)) {
+      const details = await InAppBilling.purchase(productId);
+      console.log('You purchased: ', details);
+    }
+    const transactionStatus = await InAppBilling.getPurchaseTransactionDetails(productId);
+    console.log('Transaction Status', transactionStatus);
+    const productDetails = await InAppBilling.getProductDetails(productId);
+    console.log(productDetails);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await InAppBilling.consumePurchase(productId);
+    await InAppBilling.close();
+  }
+}
 
 const Vip = inject("SubjectStore")(observer(({ SubjectStore }) => {
   
@@ -12,6 +35,7 @@ const Vip = inject("SubjectStore")(observer(({ SubjectStore }) => {
         checked={SubjectStore.vip}
         onPress={SubjectStore.setVip.bind(SubjectStore)}
       />
+      <Button onPress={pay} title="我要升級" color="#841584" accessibilityLabel="Learn more about this purple button" />
     </View>
   )
 }))
