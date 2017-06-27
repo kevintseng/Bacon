45ceb62df24bbf41cdd90102152cca1271574afa
Tsao <tsaohucn@gmail.com>
2017-06-27 16:11:04 +0800

    modify SideBar

--- src/app/views/SideBar.js
+++ src/app/views/SideBar.js
@@ -71,7 +71,7 @@ const SideBar = observer(({ store }) => {
             roundAvatar
             containerStyle = { listItemContainerStyle }
             avatar = { store.user.photoURL ? store.user.photoURL : loading}
-            title = { <Text>{store.user.displayName}</Text> }
+            title = { <Text>  {store.user.displayName}</Text> }
             onPress = { store.handleOnPress('aboutme') }
         />
         
