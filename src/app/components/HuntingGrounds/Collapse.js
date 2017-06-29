b21b4eed358e8282728b5e3e2faca4b091b91fb3
Tsao <tsaohucn@gmail.com>
2017-06-29 22:43:36 +0800

    modify photos

--- src/app/components/HuntingGrounds/Collapse.js
+++ src/app/components/HuntingGrounds/Collapse.js
@@ -6,7 +6,7 @@ import { observer, inject } from "mobx-react/native"
 
 const Collapse = inject("ObjectStore")(observer(({ ObjectStore, leftIcon, rightIcon, showbutton }) => {
 
-  const photoURL = 'https://firebasestorage.googleapis.com/v0/b/kjyl-150415.appspot.com/o/collapse.jpg?alt=media&token=020621ab-6347-46ef-9b65-97beb6ce80f4'
+  //const photoURL = 'https://firebasestorage.googleapis.com/v0/b/kjyl-150415.appspot.com/o/collapse.jpg?alt=media&token=020621ab-6347-46ef-9b65-97beb6ce80f4'
 
   const renderButton = (
     <View style={{ flex: 1, marginLeft: 20, flexDirection: "row", alignItems: "flex-end", justifyContent: "center"}}>
@@ -16,28 +16,30 @@ const Collapse = inject("ObjectStore")(observer(({ ObjectStore, leftIcon, rightI
   )
 
   const unit = (
-        <Image resizeMode="contain" style={{flex: 1}} source={{uri: photoURL}}>
+        <Image key={ObjectStore.photoURL} resizeMode="contain" style={{flex: 1}} source={{uri: ObjectStore.photoURL}}>
           <View style={{ flexDirection: "row", alignItems: "flex-end", marginRight: 10, marginTop: 10 }}>
             <View style={{ flex: 1 }}></View>
-            <View style={{ borderWidth: 3, borderColor: "#f0f0f0", padding: 7, borderRadius: 7 }} ><Text style={{color: "#f0f0f0"}}>{ObjectStore.photos.length}</Text></View>
+            <View style={{ borderWidth: 3, borderColor: "#f0f0f0", padding: 7, borderRadius: 7 }} ><Text style={{color: "#f0f0f0"}}>{ObjectStore.photos.length + 1}</Text></View>
           </View>
           { showbutton && renderButton }
         </Image>
   )
 
-  const renderCollapse = ObjectStore.photos.map((photo) => ( 
+  const Collapse = ObjectStore.photos.map((photo) => ( 
         <Image key={photo.src.uri} resizeMode="contain" style={{flex: 1, alignItems: "center"}} source={{uri: photo.src.uri}}>
           <View style={{ flexDirection: "row", alignItems: "flex-end", marginRight: 10, marginTop: 10 }}>
             <View style={{ flex: 1 }}></View>
-            <View style={{ borderWidth: 3, borderColor: "#f0f0f0", padding: 7, borderRadius: 7 }} ><Text style={{color: "#f0f0f0"}}>{ObjectStore.photos.length}</Text></View>
+            <View style={{ borderWidth: 3, borderColor: "#f0f0f0", padding: 7, borderRadius: 7 }} ><Text style={{color: "#f0f0f0"}}>{ObjectStore.photos.length + 1}</Text></View>
           </View>
           { showbutton && renderButton }
         </Image> ))
 
+  const renderCollapse = [unit].concat(Collapse)
+
   return(
     <View style={{flex: 1}}>
       <Carousel delay={2000} style={{ flex: 1 }} pageInfo autoplay={false}> 
-        { renderCollapse.length > 0 ? renderCollapse : unit }
+        { renderCollapse }
       </Carousel> 
     </View>
   )
