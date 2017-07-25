735908b7efc11538d60b67a1177411f43d0f396d
Tsao <tsaohucn@gmail.com>
2017-07-25 17:03:15 +0800

    rebuild ios

--- ios/Pods/Target Support Files/Pods-Bacon/Pods-Bacon-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bacon : NSObject
-@end
-@implementation PodsDummy_Pods_Bacon
-@end
--- ios/Pods/Target Support Files/Pods-BaconTests/Pods-BaconTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BaconTests : NSObject
-@end
-@implementation PodsDummy_Pods_BaconTests
-@end
