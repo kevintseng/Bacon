bb494f313a16c55ad192c1537ee0353d08cab2a9
Tsao <tsaohucn@gmail.com>
2017-07-31 16:47:47 +0800

    re-arch

---
+++ src/app/containers/FateTab/CollectionContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class CollectionContainer extends Component {
+
+  render() {
+    return(
+      <View>
+        <Text>CollectionContainer</Text>
+      </View>
+    )
+  }
+}
