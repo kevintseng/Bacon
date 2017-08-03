ff567d5a1b27ed0407090999d985fcbf65b2759b
Tsao <tsaohucn@gmail.com>
2017-08-03 19:24:19 +0800

    fix

--- src/app/scenes/drawer/MeetChance/MeetChanceWaterFallScene.js
+++ src/app/scenes/drawer/MeetChance/MeetChanceWaterFallScene.js
@@ -43,7 +43,7 @@ export default class MeetChanceWaterFallScene extends Component {
   }
 
   componentDidMount() {
-    //this.fetchMeetChance()
+    this.fetchMeetChance()
   }
 
   fetchMeetChance = () => {
