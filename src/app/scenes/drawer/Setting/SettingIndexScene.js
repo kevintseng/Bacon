4e90b69e15bfef2af492af6f572f91285d4347e3
Tsao <tsaohucn@gmail.com>
2017-08-08 07:33:35 +0800

    fix

--- src/app/scenes/drawer/Setting/SettingIndexScene.js
+++ src/app/scenes/drawer/Setting/SettingIndexScene.js
@@ -11,6 +11,10 @@ import Knife from '../../../views/Knife/Knife'
 
 export default class SettingIndexScene extends Component {
 
+  componentWillMount() {
+    Actions.refresh({ key: 'Drawer', open: false })
+  }
+
   render() {
     return(
       <View style={{flex: 1}}>
