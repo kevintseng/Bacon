1a70c6f751e4ff9d5f66720b8840f28d2d4b0b24
Tsao <tsaohucn@gmail.com>
2017-08-08 18:04:40 +0800

    fix bug

--- reset.sh
+++ reset.sh
@@ -3,7 +3,8 @@ watchman watch-del-all
 rm -rf $TMPDIR/react-*
 rm -rf node_modules/ 
 rm -rf package-lock.json
+rm -rf yarn.lock
 npm cache clean --force 
 yarn cache clean
-npm install
+yarn install
 npm start -- --reset-cache
