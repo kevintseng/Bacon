8a454b442e166eab9c08c0e9a2c733f86c2037ea
Tsao <tsaohucn@gmail.com>
2017-08-11 13:12:17 +0800

    fix fate avator

--- src/app/scenes/drawer/LineCollect/LineCollectRoutesScene.js
+++ src/app/scenes/drawer/LineCollect/LineCollectRoutesScene.js
@@ -3,7 +3,29 @@ import { View, Text, ActivityIndicator, ScrollView, Dimensions, BackHandler, Toa
 import { Actions } from 'react-native-router-flux'
 import { observer, inject } from 'mobx-react'
 
+import BaconRoutesContainer from '../../../containers/LineCollectRoutesScene/BaconRoutesContainer'
+
 const { width, height } = Dimensions.get('window')
+
+const styles = {
+  view: {
+    flex: 1
+  },
+  top: {
+    position: 'absolute', 
+    top: 30,
+    alignSelf: 'center',
+  },
+  middle: {
+    position: 'absolute', 
+    top: 160,
+    alignSelf: 'center',
+  },
+  bottom: {
+    position: 'absolute', 
+    bottom: 0
+  }
+}
 
 @inject('firebase','SubjectStore','MeetChanceStore') @observer
 export default class LineCollectRoutesScene extends Component {
@@ -12,16 +34,15 @@ export default class LineCollectRoutesScene extends Component {
     super(props)
     this.firebase = this.props.firebase
   }
-
-  goToBonus= () => {
-    Actions.Bonus()
-  }
-
+  
   render() {
     return(
-      <View style={{flex: 1}}>
-        <Text>LineCollectRoutes</Text>
-        <Button title='轉到儲值業面' onPress={this.goToBonus}/>
+      <View style={ styles.view }>
+
+        <View style={ styles.bottom }>
+          <BaconRoutesContainer/>
+        </View>
+
       </View>
     )
   }
