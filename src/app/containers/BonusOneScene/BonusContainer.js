841a681d778dc579bc59c33b80843f590642d54f
Tsao <tsaohucn@gmail.com>
2017-08-14 14:08:34 +0800

    upgrade UI

--- src/app/containers/BonusOneScene/BonusContainer.js
+++ src/app/containers/BonusOneScene/BonusContainer.js
@@ -3,19 +3,54 @@ import { View, TouchableOpacity, Text,Button } from 'react-native'
 import { inject, observer } from 'mobx-react'
 import RNGooglePlaces from 'react-native-google-places'
 
-import Bonus from '../../views/Bonus'
+import Bonus from '../../views/Bill/Bonus'
 
 @inject('SignUpStore') @observer
 export default class BonusContainer extends Component {
 
   constructor(props) {
     super(props)
-    this.SignUpStore = this.props.SignUpStore
+    this.state={
+      topCheck: true,
+      middleCheck: false,
+      upperCheck: false
+    }
+  }
+
+  topCheckOnPress = () => {
+    this.setState({
+      topCheck: true,
+      middleCheck: false,
+      upperCheck: false
+    })
+  }
+
+  middleCheckOnPress = () => {
+    this.setState({
+      topCheck: false,
+      middleCheck: true,
+      upperCheck: false
+    })    
+  }
+
+  upperCheckOnPress = () => {
+    this.setState({
+      topCheck: false,
+      middleCheck: false,
+      upperCheck: true
+    })    
   }
 
   render() {
     return(
-      <Bonus/>
+      <Bonus
+        topCheck={ this.state.topCheck }
+        middleCheck={ this.state.middleCheck }
+        upperCheck={ this.state.upperCheck }
+        topCheckOnPress={ this.topCheckOnPress }
+        middleCheckOnPress={ this.middleCheckOnPress }
+        upperCheckOnPress={ this.upperCheckOnPress }
+      />
     )
   }
 }
--- src/app/containers/UpgradeOneScene/UpgradeContainer.js
+++ src/app/containers/UpgradeOneScene/UpgradeContainer.js
@@ -3,19 +3,42 @@ import { View, TouchableOpacity, Text,Button } from 'react-native'
 import { inject, observer } from 'mobx-react'
 import RNGooglePlaces from 'react-native-google-places'
 
-import Upgrade from '../../views/Upgrade'
+import Upgrade from '../../views/Bill/Upgrade'
 
 @inject('SignUpStore') @observer
 export default class UpgradeContainer extends Component {
 
   constructor(props) {
     super(props)
-    this.SignUpStore = this.props.SignUpStore
+    //this.SignUpStore = this.props.SignUpStore
+    this.state={
+      topCheck: true,
+      upperCheck: false
+    }
+  }
+
+  topCheckOnPress = () => {
+    this.setState({
+      topCheck: !this.state.topCheck,
+      upperCheck: !this.state.upperCheck
+    })
+  }
+
+  upperCheckOnPress = () => {
+    this.setState({
+      topCheck: !this.state.topCheck,
+      upperCheck: !this.state.upperCheck
+    })    
   }
 
   render() {
     return(
-      <Upgrade/>
+      <Upgrade
+        topCheck={ this.state.topCheck }
+        upperCheck={ this.state.upperCheck }
+        topCheckOnPress={ this.topCheckOnPress }
+        upperCheckOnPress={ this.upperCheckOnPress }
+      />
     )
   }
 }
