315eaa3d32c0ce1ebbfbafbaf19195b2335de1c3
Tsao <tsaohucn@gmail.com>
2017-08-16 19:17:39 +0800

    fix meetchance

--- src/app/views/Cookie/Cookie.js
+++ src/app/views/Cookie/Cookie.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Image, Text, Dimensions, TouchableOpacity } from 'react-native'
 import FastImage from 'react-native-fast-image'
 import { Avatar, Badge } from 'react-native-elements'
+import MKPLoadImageView from 'mkp-react-native-image-view'
 
 const { width } = Dimensions.get('window')
 
@@ -9,32 +10,53 @@ const x = 5
 
 const picWidth = (width - 4 * x)/3
 
-const ADD_IMAGE = require('Bacon/src/images/addImage.png')
+const DEFAULT_IMAGE = require('./img/ico_qy_head_preload.png')
 
 const Cookie = ({ name, size, avatar, onPress }) => {
 
+const circleSize = size || picWidth
+
+const circleFixBorder = circleSize/2
+
+const lineBreakMode ='tail'
+
 const styles = {
-  itemImageStyle: {
-    width: size || picWidth,
-    height: size || picWidth,
-    marginBottom: 5,
-    borderRadius: size ? size/2 : picWidth*2,
+  view: {
+    alignItems: 'center', 
+    marginLeft: size ? 0 : x, 
+    marginBottom: 10,
+    //backgroundColor: 'red'
   },
-  nickname: {
-    height: 20
+  image: {
+    width: circleSize,
+    height: circleSize,
+    marginBottom: 5,
+    borderRadius: circleSize/2,
+    overflow: 'hidden'
+  },
+  indicatorProps: {
+    color:'red',
+    style:
+    {
+      margin:10,
+      flex:1
+    }
+  },
+  avatar: {
+    uri: avatar
+  },
+  text: {
+    lineHeight: 20
   }
 }
 
   return(
-    <View style={{alignItems: 'center', marginLeft: size ? 0 : x, marginBottom: 10}}>
-      <TouchableOpacity activeOpacity={0.8} onPress={onPress}>
-        <Image source={ avatar ? { uri: avatar } : require('./img/ico_qy_head_preload.png') } style={styles.itemImageStyle}/>
-      </TouchableOpacity>
-      <View style={styles.nickname}>
-        <Text lineBreakMode="tail" numberOfLines={1} >{ name }</Text>
-      </View>
+    <View style={styles.view}>
+      <MKPLoadImageView hiddenProgress={true} style={ styles.image } source={ styles.avatar } defaultSource={ DEFAULT_IMAGE } onPress={ onPress }/>
+      <Text style={ styles.text } lineBreakMode={ lineBreakMode } numberOfLines={1} >{ name }</Text>
     </View>
   )
 }
 
 export default Cookie
+// activeOpacity={0.8} onPress={onPress}
