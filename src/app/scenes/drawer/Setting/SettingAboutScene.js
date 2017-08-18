559777ea1b3145f5c905f22bad190cd29b0807b6
Tsao <tsaohucn@gmail.com>
2017-08-18 11:03:02 +0800

    update version number

--- src/app/scenes/drawer/Setting/SettingAboutScene.js
+++ src/app/scenes/drawer/Setting/SettingAboutScene.js
@@ -36,7 +36,7 @@ export default class SettingAboutScene extends Component {
 
   version = () => {
     Alert.alert( 
-      '目前版本', 'Alpha-1.1.4', [ 
+      '目前版本', 'Alpha-1.1.5', [ 
         {text: '確認', onPress: () => console.log('OK Pressed')}, ], { cancelable: false } 
       )
   }
