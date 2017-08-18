4afb6e5b73d34635b7a86c696fb3acbe005af9df
KTseng <tgpsstar@gmail.com>
2017-08-18 16:24:36 +0800

    Changed Modal content switching method

--- src/app/containers/LineListScene/CustomStatusInputModalContainer.js
+++ src/app/containers/LineListScene/CustomStatusInputModalContainer.js
@@ -19,19 +19,17 @@ export default class CustomStatusInputModalContainer extends Component {
     super(props)
     this.state = {
       selfInputChatStatus: null,
-      showModal: this.props.showModal,
     }
   }
 
   handleSelfInputCancel = () => {
     this.setState({
-      showModal: false,
       selfInputChatStatus: "",
     })
+    this.props.onCancel()
   }
 
   handleSelfInputSubmit = () => {
-    this.setState({showModal: false})
     this.props.onSubmit(this.state.selfInputChatStatus)
   }
 
@@ -43,53 +41,46 @@ export default class CustomStatusInputModalContainer extends Component {
 
   render() {
     return (
-      <View>
-        <Modal
-          style={{ alignItems: "center" }}
-          isVisible={this.state.showModal}
+      <View
+        style={modalContentStyle}
+      >
+        <Text style={{ marginTop: 25, fontWeight: "600", fontSize: 16 }}>
+          請輸入自訂狀態
+        </Text>
+        <TextInput
+          maxLength={3}
+          onChangeText={text => this.textInputOnChange(text)}
+          defaultValue={this.state.selfInputChatStatus}
+          style={{
+            marginVertical: 25,
+            height: 40,
+            width: 200,
+            borderColor: "#B3B3B3",
+            borderWidth: 1,
+            borderRadius: 5,
+          }}
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            width: 300,
+            justifyContent: "space-between",
+            paddingHorizontal: 50,
+          }}
         >
-          <View
-            style={modalContentStyle}
-          >
-            <Text style={{ marginTop: 25, fontWeight: "600", fontSize: 16 }}>
-              請輸入自訂狀態
-            </Text>
-            <TextInput
-              maxLength={3}
-              onChangeText={text => this.textInputOnChange(text)}
-              defaultValue={this.state.selfInputChatStatus}
-              style={{
-                marginVertical: 25,
-                height: 40,
-                width: 200,
-                borderColor: "#B3B3B3",
-                borderWidth: 1,
-                borderRadius: 5,
-              }}
-            />
-            <View
-              style={{
-                flexDirection: "row",
-                width: 300,
-                justifyContent: "space-between",
-                paddingHorizontal: 50,
-              }}
-            >
-              <Button
-                title="取消"
-                onPress={() => this.handleSelfInputCancel()}
-                color="#0076FF"
-                buttonStyle={{ backgroundColor: "transparent" }}
-              />
-              <Button
-                title="送出"
-                onPress={() => this.handleSelfInputSubmit()}
-                color="#0076FF"
-                buttonStyle={{ backgroundColor: "transparent" }}
-              />
-            </View>
-          </View>
-        </Modal>
+          <Button
+            title="取消"
+            onPress={() => this.handleSelfInputCancel()}
+            color="#0076FF"
+            buttonStyle={{ backgroundColor: "transparent" }}
+          />
+          <Button
+            title="送出"
+            onPress={() => this.handleSelfInputSubmit()}
+            color="#0076FF"
+            buttonStyle={{ backgroundColor: "transparent" }}
+          />
+        </View>
       </View>
     )
   }
