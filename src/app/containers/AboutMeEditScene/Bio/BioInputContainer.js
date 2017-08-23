a4f90ab624dd26ab4338c71570662d7461f8f0e9
Tsao <tsaohucn@gmail.com>
2017-08-23 17:53:16 +0800

    aboutMe

--- src/app/containers/AboutMeEditScene/Bio/BioInputContainer.js
+++ src/app/containers/AboutMeEditScene/Bio/BioInputContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { View, TextInput, Dimensions, ScrollView } from 'react-native'
+import { Keyboard, View, Button, TextInput, Dimensions, ScrollView, TouchableWithoutFeedback } from 'react-native'
 import { Actions } from 'react-native-router-flux'
-import { inject, observer } from "mobx-react"
+import { inject, observer } from 'mobx-react'
 
 import BaconForm from '../../../views/BaconForm'
 
@@ -10,7 +10,8 @@ const { width, height } = Dimensions.get('window')
 const styles = {
   textInput: {
     width: width - 60,
-    alignItems: 'center'
+    alignItems: 'center',
+    fontSize: 18
   }
 }
 
@@ -22,24 +23,35 @@ export default class BioInputContainer extends Component {
     this.SubjectEditStore = this.props.SubjectEditStore
   }
 
+  ensure = () => {
+    Keyboard.dismiss()
+  }
+
+  clear = () => {
+    this.refs['bio'].clear()
+  }
+
   render() {
     return(
       <View style={{alignSelf: 'center'}}>
-        <ScrollView scrollEnabled={false}>
-          <TextInput
-            style={styles.textInput}
-            underlineColorAndroid="#d63768"
-            textAlignVertical = 'top'
-            placeholder = '請輸入自我介紹'
-            multiline
-            autoCorrect
-            numberOfLines = { 100 }
-            //editable = { true }
-            maxLength = { 100 }
-            onChangeText = { this.SubjectEditStore.setBio }
-            value = {this.SubjectEditStore.bio }
-          />
-        </ScrollView>
+          <View style={{height: 300,borderColor: '#D63768',borderWidth: 1, borderRadius: 5}}>
+            <TextInput
+              ref={'bio'}
+              style={styles.textInput}
+              textAlignVertical = 'top'
+              placeholder = '請輸入自我介紹'
+              multiline
+              autoFocus
+              numberOfLines = { 100 }
+              maxLength = { 250 }
+              onChangeText = { this.SubjectEditStore.setBio }
+              value = {this.SubjectEditStore.bio }
+            />
+          </View>
+          <View style={{flexDirection: 'row',justifyContent: 'space-between'}}>
+            <Button title='清除' onPress={this.clear}/>
+            <Button title='確認' onPress={this.ensure}/>
+          </View>
       </View>
     )
   }
