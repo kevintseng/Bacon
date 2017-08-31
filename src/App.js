8a4cd3239e0fa11cff03b403b1e8fdfd2b840932
Tsao <tsaohucn@gmail.com>
2017-08-31 18:59:45 +0800

    lock Orientation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import Orientation from 'react-native-orientation-locker'
 import { Provider } from 'mobx-react/native'
 // initial config
 import AppInitial from './configs/AppInitial'
 import Routes from './app/Routes'
 
 export default class App extends Component {
+
+  componentWillMount() {
+    Orientation.lockToPortrait()
+  }
 
   render() {
     return (
