6c9c555c2c5a62687609c0281ab39f385ff469ea
Tsao <tsaohucn@gmail.com>
2017-09-05 18:35:01 +0800

    login out

--- src/app/containers/SettingAccountScene/LoginOutContainer.js
+++ src/app/containers/SettingAccountScene/LoginOutContainer.js
@@ -3,19 +3,35 @@ import { inject, observer } from "mobx-react"
 
 import GreyButton from '../../views/GreyButton/GreyButton'
 
-@inject('firebase') @observer
+@inject('firebase','SubjectStore') @observer
 export default class LoginOutContainer extends Component {
 
   constructor(props) {
     super(props)
     this.firebase = this.props.firebase
-    this.user = this.firebase.auth().currentUser
+    this.SubjectStore = this.props.SubjectStore
+    //this.user = this.firebase.auth().currentUser
   }
 
   _loginOut = () => {
-    this.firebase.auth().signOut().catch( err => {
-      console.log(err)
-      alert('登出發生錯誤')
+    this.firebase.database().ref('/online/' + this.SubjectStore.uid).once('value',snap => {
+      const lastOnline = snap.val().lastOnline
+      const location = snap.val().location
+      this.firebase.database().ref('/online/' + this.SubjectStore.uid).remove()
+      .then(() => {
+        this.firebase.auth().signOut().catch( err => {
+          this.firebase.database().ref('/online/' + this.SubjectStore.uid).set({
+            lastOnline: lastOnline,
+            location: location
+          })
+          console.log(err)
+          alert('登出發生錯誤')
+        })
+      })
+      .catch(err => { 
+        alert('登出發生錯誤')
+        console.log(err) 
+      })
     })
   }
 
