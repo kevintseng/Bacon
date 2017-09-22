c7bf41a4ae11f40df826c3ac6da8c0d3285eeea8
Tsao <tsaohucn@gmail.com>
2017-09-22 13:31:56 +0800

    add article

--- src/app/scenes/drawer/Setting/SettingAboutScene.js
+++ src/app/scenes/drawer/Setting/SettingAboutScene.js
@@ -43,7 +43,7 @@ export default class SettingAboutScene extends Component {
 
   version = () => {
     Alert.alert(
-      '目前版本', 'Beta-1.2.0', [
+      '目前版本', 'Beta-1.3.0', [
         {text: '確認', onPress: () => console.log('OK Pressed')}, ], { cancelable: false }
       )
   }
