9ac56252c5502bb04519b409276604b9615c2970
Tsao <tsaohucn@gmail.com>
2017-10-02 15:41:47 +0800

    master

--- src/app/containers/MasterTabScene/ReservationContainer.js
+++ src/app/containers/MasterTabScene/ReservationContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { View, Text, FlatList, TouchableOpacity } from 'react-native'
 
+import ReservationList from '../../views/ReservationList'
+
 export default class ReservationContainer extends Component {
 
   constructor(props) {
@@ -11,8 +13,11 @@ export default class ReservationContainer extends Component {
   render() {
     return(
       <View>
-        <Text>ReservationContainer</Text>
-      </View>
+        <ReservationList
+          title={'情傷療癒系作家 艾姬'}
+          time={'2017-09-15 10:30~11:00'}
+        />
+      </View> 
       )
   }
 }
---
+++ src/app/views/ReservationList.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import { View, Image, Text, Dimensions, TouchableOpacity } from 'react-native'
+import { BaconBadgeYes, BaconBadgeNo } from './BaconBadge/BaconBadge'
+
+import Cookie from './Cookie/Cookie'
+
+const DEFAULT_IMAGE = require('./Cookie/img/ico_qy_head_preload.png')
+
+const styles = {
+  article: {
+    flexDirection: 'row',
+    margin: 10,
+    height: 100,
+    borderBottomWidth: 1,
+    borderColor: '#b3b3b3'
+  },
+  image: {
+    width: 90,
+    height: 90
+  },
+  titleView: {
+    paddingLeft: 10,
+    justifyContent: 'center'
+  },
+  title: {
+    width: 240,
+    letterSpacing: 3,
+    fontFamily: 'NotoSans',
+    fontSize: 18,
+    fontWeight: '400',
+    color: '#606060',
+    backgroundColor: 'transparent'
+  },
+  content: {
+    flexDirection: 'row',
+    paddingLeft: 10,
+    justifyContent: 'center'
+  },
+  avatar: {
+    width: 30,
+    height: 30,
+    borderRadius: 15   
+  },
+  bottom: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingRight: 5
+  },
+  tags: {
+    flexDirection: 'row'
+  },
+  button: {
+    justifyContent: 'center'
+   // flexDirection: 'row'
+  }
+}
+
+const ReservationList = ({title,time}) => {
+
+  const showTags = () => ( 
+    tags.map( tag => (
+      <BaconBadgeYes key={tag} text={tag}/>
+    ))
+  )
+
+
+  return(
+    <View style={styles.article}>
+      <View style={styles.content}>
+        <View style={styles.titleView}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.title}>{time}</Text>
+        </View>
+        <View style={styles.button}>
+          <BaconBadgeYes key={'請進'} text={'請進'}/>
+          <BaconBadgeYes key={'取消'} text={'取消'}/>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default ReservationList
