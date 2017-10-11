2fda3f18dd849a848abb07e1ce89445c4fd88582
KTseng <tgpsstar@gmail.com>
2017-10-11 19:31:01 +0800

    Start refactoring LINE

--- src/app/containers/LineTabScene/UnreadContainer.js
+++ src/app/containers/LineTabScene/UnreadContainer.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react'
 import { View, Text, FlatList, TouchableOpacity } from 'react-native'
 
-import ReservationList from '../../views/ReservationList'
+import LineList from '../../views/LineList'
 
-export default class ReservationContainer extends Component {
+export default class UnreadContainer extends Component {
 
   constructor(props) {
     super(props)
+    this.firebase = this.props.firebase
   }
-
 
   render() {
     return(
       <View>
-        <ReservationList
+        <LineList
           title={'情傷療癒系作家 艾姬'}
           time={'2017-09-15 10:30~11:00'}
         />
       </View>
-      )
+    )
   }
 }
