c187f8dcbee9435bae81b9945515aad99c507570
Tsao <tsaohucn@gmail.com>
2017-10-20 16:07:59 +0800

    chat UI

--- src/app/containers/ChatTabScene/AllChatContainer.js
+++ src/app/containers/ChatTabScene/AllChatContainer.js
@@ -33,10 +33,24 @@ export default class AllChatContainer extends Component {
           name={'Dora Li'}
           avatar={{uri: 'http://img.appledaily.com.tw/images/ReNews/20170115/640_86387ec286267a13d6d6d0e82606b39d.jpg'}}
           onPress={ this.onPress }
-          lastChatContent={'發了一張圖片'}
+          lastChatContent={'誰答腔我就罵誰'}
+          nonHandleChatCount={99}
+          showBadge
+          showTag
+          online
+          userState={'平淡中'}
+          userStateColor={'#FFD306'}
+        />
+        <ChatList
+          name={'大笨蛋'}
+          avatar={{uri: 'https://cdn2.ettoday.net/images/2322/2322944.jpg'}}
+          onPress={ this.onPress }
+          lastChatContent={'真的很想很想你'}
           nonHandleChatCount={2}
           showBadge
-          //showTag
+          showTag
+          userState={'忙碌中'}
+          userStateColor={'#FF8040'}
         />
       </View>
     )
