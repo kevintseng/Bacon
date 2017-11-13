d4cfa642f77928964372be73225d71d1f517fcc0
Tsao <tsaohucn@gmail.com>
2017-11-13 18:34:54 +0800

    chat rooms

--- src/app/views/Sticker.js
+++ src/app/views/Sticker.js
@@ -31,6 +31,56 @@ const Sticker = ({onPress}) => {
         <TouchableOpacity style={{padding: 5}} onPress={onPress}>
           <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fboy08.png?alt=media&token=79c1c610-4de6-4402-89f3-a348f9f8a501'}} />
         </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fboy09.png?alt=media&token=46961f8c-6dde-42fd-9f16-af732fdf90ce'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fboy10.png?alt=media&token=0d6b4860-bbfd-4c07-8d86-830a1acb4ef9'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fboy11.png?alt=media&token=56a9090b-9f60-4e1e-a7bb-89f1ddb7968a'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fboy12.png?alt=media&token=1a9fa7c5-0208-4d20-8dde-04602655e290'}} />
+        </TouchableOpacity>
+
+
+        <TouchableOpacity  style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl01.png?alt=media&token=8b62aa65-3c7a-4888-b4ac-7615ad290c13'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl02.png?alt=media&token=815e7f25-b592-49e1-b50c-06df3484ceb8'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl03.png?alt=media&token=7c23a617-9ecf-457b-8661-9c64ec758745'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl03.png?alt=media&token=7c23a617-9ecf-457b-8661-9c64ec758745'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl03.png?alt=media&token=7c23a617-9ecf-457b-8661-9c64ec758745'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl06.png?alt=media&token=03cf1bb8-d3b9-4c23-b72c-dfe251620014'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl07.png?alt=media&token=a27c6f6b-6879-467f-8cdd-bf986ec0b5be'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl07.png?alt=media&token=a27c6f6b-6879-467f-8cdd-bf986ec0b5be'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl09.png?alt=media&token=14d2f672-09b3-4033-8465-e6658e0f7e48'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl10.png?alt=media&token=e435d204-fc7d-4bf6-9c7e-eec4a3e4bb64'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl11.png?alt=media&token=a3ea35bc-9cd7-4568-8642-4baead084bf3'}} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 5}} onPress={onPress}>
+          <Image style={{width: 74, height: 64}} source={{uri: 'https://firebasestorage.googleapis.com/v0/b/bacon-fake.appspot.com/o/BaconSticker%2Fgirl12.png?alt=media&token=fa62ef65-f542-4fb5-9ba2-ecfce57ef82f'}} />
+        </TouchableOpacity>
       </View>
     </ScrollView>
   )
