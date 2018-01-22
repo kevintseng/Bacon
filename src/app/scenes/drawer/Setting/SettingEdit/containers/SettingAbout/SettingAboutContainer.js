190507dbbf6fb85f9d0b9df82bcee7947d523881
Tsao <tsaohucn@gmail.com>
2018-01-22 22:02:02 +0800

    Beta-4.7.0

--- src/app/scenes/drawer/Setting/SettingEdit/containers/SettingAbout/SettingAboutContainer.js
+++ src/app/scenes/drawer/Setting/SettingEdit/containers/SettingAbout/SettingAboutContainer.js
@@ -43,7 +43,7 @@ export default class SettingAboutContainer extends Component {
 
   version = () => {
     Alert.alert(
-      '目前版本', 'Beta-4.6.0', [
+      '目前版本', 'Beta-4.7.0', [
         {text: '確認', onPress: () => console.log('OK Pressed')}, ], { cancelable: false }
       )
   }
