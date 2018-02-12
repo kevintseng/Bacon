cd1e4e8844ff91e847a5e12f4f7ad4d575c5f40d
Tsao <tsaohucn@gmail.com>
2018-02-12 19:08:55 +0800

    modify UI

--- src/app/views/Knife/Knife.js
+++ src/app/views/Knife/Knife.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import { View, Image } from 'react-native'
+import { View, Image, Dimensions } from 'react-native'
+
+const { width, height } = Dimensions.get("window") //eslint-disable-line
+
+const pxToDp = (uiElementPx) => {
+  //PixelRatio.get();
+  return uiElementPx*(width/1440)
+}
+
+const styles = {
+  knife: {
+    width: pxToDp(1440), 
+    height: pxToDp(388)
+  }
+}
 
 const Knife = () => {
   return(
     <View>
-      <Image source={require('./img/bg_setting.png')} />
+      <Image style={styles.knife} source={require('./img/bg_setting.png')} />
     </View>
   )
 }
