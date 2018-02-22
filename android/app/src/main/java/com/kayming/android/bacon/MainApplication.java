1340e1dd998463c484d0c65c00aef79549aab493
Tsao <tsaohucn@gmail.com>
2018-02-23 01:12:20 +0700

    Revert "react-native-fcm"
    This reverts commit 62d9b35737ba89e1c0f25c7f925f6bfc53b30c2a.

--- android/app/src/main/java/com/kayming/android/bacon/MainApplication.java
+++ android/app/src/main/java/com/kayming/android/bacon/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import com.horcrux.svg.SvgPackage;
 import com.idehub.Billing.InAppBillingBridgePackage;
 import com.facebook.react.ReactApplication;
-import com.evollu.react.fcm.FIRMessagingPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.wonday.orientation.OrientationPackage;
 import com.rnfs.RNFSPackage;
@@ -38,7 +37,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FIRMessagingPackage(),
             new VectorIconsPackage(),
             new OrientationPackage(),
             new RNFSPackage(),
